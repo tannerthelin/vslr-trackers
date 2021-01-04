20364591e480204694a6f453d32e8139cc248ffe
tannerthelin <tannerthelin@gmail.com>
2021-01-04 14:46:30 -0700

    Changed coast keys to all caps

--- pages/one_wheel_incentive/club_east/index.js
+++ pages/one_wheel_incentive/club_east/index.js
@@ -9,7 +9,7 @@ simpleSheet: true } )
 function showInfo(data, tabletop) {
 for (var i = 0; i < data.length; i++) {
 	// Looping through each row of the spreadsheet
-	if (data[i].league_level === "CLUB" && data[i].coast === "east") {		
+	if (data[i].league_level === "CLUB" && data[i].coast === "EAST") {		
 		$("#table-body").append(
 		'<tr class="row">' +
 		'<td class="name-column">' + data[i].name + '</td>' +
--- pages/one_wheel_incentive/franchise_west/index.js
+++ pages/one_wheel_incentive/franchise_west/index.js
@@ -9,7 +9,7 @@ simpleSheet: true } )
 function showInfo(data, tabletop) {
 for (var i = 0; i < data.length; i++) {
 	// Looping through each row of the spreadsheet
-	if (data[i].league_level === "FRANCHISE" && data[i].coast === "west") {		
+	if (data[i].league_level === "FRANCHISE" && data[i].coast === "WEST") {		
 		$("#table-body").append(
 		'<tr class="row">' +
 		'<td class="name-column">' + data[i].name + '</td>' +
