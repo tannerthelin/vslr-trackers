04d781781023b1b11bfca09603603c7a250bf598
tannerthelin <tannerthelin@gmail.com>
2021-01-04 12:16:35 -0700

    Added v1 of One Wheel Incentive

---
+++ pages/one_wheel_incentive/combine_west/index.js
@@ -0,0 +1,21 @@
+function init() {
+Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1jDlsUV2mjoN85WLZhDFceB_eLFvtCA6BdA2oHI4zz1U/edit?usp=sharing',
+callback: function(data, tabletop) { 
+console.log(data)
+},
+simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+for (var i = 0; i < data.length; i++) {
+	// Looping through each row of the spreadsheet
+	if (data[i].league_level === "combine" && data[i].coast === "west") {		
+		$("#table-body").append(
+		'<tr class="row">' +
+		'<td class="name-column">' + data[i].name + '</td>' +
+		'<td class="last">' + data[i].score + '</td>' +
+		'</tr>'
+		); 
+	}
+} 
+}
