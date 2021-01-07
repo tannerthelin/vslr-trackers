a196804f422815258a6075583c83ed7c7724be84
tannerthelin <tannerthelin@gmail.com>
2021-01-07 10:42:39 -0700

    Adding IL and CA dealers to VIP Tracker

--- pages/league-trip-vip/west/index.js
+++ pages/league-trip-vip/west/index.js
@@ -8,10 +8,10 @@ function init() {
 	
 	function showInfo(data, tabletop) {
 	
-		var atomic = ["CA-04", "CA-09", "CA-23"];
+		var atomic = ["CA-04", "CA-09", "CA-23", "CA-Solterra Dealer"];
 		var core = ["CA-18", "CA-07", "CA-01", "CA-05", "CA-30"];
 		var flow = ["CA-14", "CA-02", "CA-13", "CA-15"];
-		var grit = ["IL-05", "NJ-02", "NJ-01", "IL-04", "PA-02", "NY-05", "NY-03", "NY-02", "IL-02", "NJ-03", "NJ-08", "NJ-11", "NJ-12", "PA-02"];
+		var grit = ["IL-05", "NJ-02", "NJ-01", "IL-04", "PA-02", "NY-05", "NY-03", "NY-02", "IL-02", "NJ-03", "NJ-08", "NJ-11", "NJ-12", "PA-02", "IL-Solterra Dealer"];
 		var legion = ["VA-01", "FL-01", "MD-02", "MD-01", "MD-04"];1
 		var new_england = ["MA-01", "CT-01", "MA-05", "MA-02", "MA-03", "MA-04", "CT-03"];
 		var originals = ["CA-21", "CA-10", "CA-12", "CA-06", "CA-36"];
